5d8b760f24ca7e450b26fbe3a58334988ad3bbc4
Matu36 <matipineda85@live.com.ar>
2022-11-11 16:53:34 -0300

    TESTING

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -1,34 +1,45 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const { sumArray } = require("./extra");
+const {sumaArray} = require ("./sumArray");
 
 app.use(express.json()); // for parsing application/json
 
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: "hola",
-  });
-});
-app.get("/test", (req, res) => {
-  res.status(200).send({
-    message: "test",
+app.get('/', (req, res) => {
+  res.send({
+    message: `hola`,
   });
 });
 
-app.post("/sum", (req, res) => {
+
+app.get('/test', (req, res) => {
   res.send({
-    result: req.body.a + req.body.b,
+    message: `test`,
   });
 });
-app.post("/product", (req, res) => {
+
+app.post ('/sum', (req, res) => {
+ const {a, b} = req.body;
+ res.send ({result: a + b})
+  })
+  
+
+app.post('/product', (req, res) => {
   res.send({
     result: req.body.a * req.body.b,
   });
 });
 
-app.post("/sumArray", (res, req) => {
-  const { array, num } = req.body;
-  res.status(200).send({ result: sumArray(array, num) });
-});
+
+app.post ('/sumArray', (req, res) => {
+    const {array, num} = req.body;
+    const result = sumaArray (array, num)
+    res.send ({
+      result: result
+    })
+  });
+
+
+  
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
+
